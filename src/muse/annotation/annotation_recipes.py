229fd8e09c11a6c8319ed4a3a3188ebbde570b9b
Laure Thompson <602628+laurejt@users.noreply.github.com>
2026-03-24 14:28:12 -0400

    Refined recipe
    - Displays language for original text
    - Set the history_text for each example
    - Add option to include instructions
    - Updated Q1 prompt
    - Added evaluation typology to Q2
    - Removed stats from progress panel
    - Add validation for when Q3 response is required

--- src/muse/annotation/annotation_recipes.py
+++ src/muse/annotation/annotation_recipes.py
@@ -9,6 +9,7 @@ Example Usage:
     prodigy concept-eval muse_concepts notion-concept-tasks.jsonl -F annotation_recipes.py
 """
 
+import pathlib
 from collections import Counter
 from collections.abc import Iterator
 
@@ -20,7 +21,7 @@ from prodigy.components.session import Session
 from prodigy.components.stream import get_stream
 from prodigy.core import Arg, Controller, recipe
 from prodigy.structured_types import get_input_hash, get_task_hash
-from prodigy.types import RecipeSettingsType, StreamType
+from prodigy.types import PathInputType, RecipeSettingsType, StreamType
 from prodigy.util import INPUT_HASH_ATTR, TASK_HASH_ATTR
 
 
@@ -44,6 +45,39 @@ def add_questions(questions, stream: StreamType) -> Iterator[StreamType]:
     """
     for ex in stream:
         yield ex | {"questions": questions}
+
+
+def get_lang_name(lang_code: str):
+    """
+    Returns language name for given language code
+    """
+    if lang_code == "zh":
+        return "Chinese"
+    if lang_code == "ja":
+        return "Japanese"
+    if lang_code == "pt":
+        return "Portuguese"
+    if lang_code == "es":
+        return "Spanish"
+    raise ValueError(f"Unknown language code '{lang_code}'")
+
+
+def add_lang_name(stream: StreamType) -> Iterator[StreamType]:
+    """
+    Adds language name to items in stream
+    """
+    for ex in stream:
+        yield ex | {"lang_name": get_lang_name(ex["src_lang"])}
+
+
+def add_history_text(stream: StreamType) -> Iterator[StreamType]:
+    """
+    Adds history text to items in stream with the history text corresponding
+    to the item's term field. Note that the history_text field sets the text
+    displayed within the history panel of the web app's dashboard.
+    """
+    for ex in stream:
+        yield ex | {"history_text": ex["term"]}
 
 
 def get_lang_annotators(ctrl: Controller, lang: str) -> list[str]:
@@ -79,10 +113,16 @@ def lang_task_router(ctrl: Controller, session_id: str, item: dict) -> list[str]
     "concept-eval",
     dataset=Arg(help="Dataset to save answers to"),
     source=Arg(help="The source data as a JSONL file"),
+    instruct=Arg(
+        "--instruct",
+        "-I",
+        help="Optional intructions text or HTML file",
+    ),
 )
 def concept_eval_recipe(
     dataset: str,
     source: str,
+    instruct: PathInputType | None = None,
 ) -> RecipeSettingsType:
     # TODO: Consider adding an instruction page. See https://prodi.gy/docs/api-web-app#instructions
     log("RECIPE: Starting recipe concept-eval", locals())
@@ -90,8 +130,9 @@ def concept_eval_recipe(
     # Task elements most likely to be modified
     ## Question prompts for task
     questions = [
-        "Q1. For the following translation, highlight the translation of the concept",
-        "Q2. Evaluate the machine translation of the concept",
+        "Q1. For the following translation, identify the translation(s) of the concept.\n"
+        "Highlight all instances.",
+        "Q2. Evaluate the machine translation of the concept.",
         "Q3. Notes / observations",
     ]
 
@@ -110,19 +151,48 @@ def concept_eval_recipe(
     init_html_tmpl = "\n".join(
         [
             "<h2>Concept: {{term}}</h2>",
-            "<p><b>Source Text</b>",
+            "<p><b>Original {{lang_name}} Text</b>",
             "{{src_text}}",
             "<details>",
-            "\t<summary><b>Professional English Translation</b></summary>{{ref_text}}",
+            "<summary><b>Professional English Translation</b></summary>{{ref_text}}",
             "</details>",
             f"<hr><b>{questions[0]}</b>",
         ]
     )
+
+    q2_key = "\n".join(
+        [
+            "<details>",
+            "<summary><b>Evaluation Typology</b></summary>"
+            # Keep on same line as previous to reduce vertical whitespace
+            "<p><b>Correct translation.</b> The MT model correctly translated the"
+            " source term, either matching or approximating the professional"
+            " translation.",
+            "<p><b>Should not translate.</b> Though the MT model provided a"
+            " translation of the source term, the source term should be left in its"
+            " original language because of its untranslatability and the richness of"
+            " its affordances.",
+            "<p><b>Omitted or missing.</b> The source term is missing from the MT"
+            " model translation.",
+            "<p><b>Incorrect lexical selection.</b> The translation of the source"
+            " term is an incorrect lexical choice (Haque et al. 2019).",
+            "<p><b>Disambiguation error in target.</b> Although the MT system makes"
+            " a potentially correct lexical choice for the source term, its"
+            " translation-equivalent does not carry the meaning of the source term"
+            " (Haque et al. 2019).",
+            "<p><b>Incorrectly left untranslated.</b> The MT model did not translate"
+            " the source term when it should have.",
+            "<p><b>Other error.</b> Cases that do not fit into the above typology;"
+            " provide context in the notes section below.",
+            "</details>",
+        ]
+    )
+
     ## Arrangement of combined interfaces
     blocks = [
         {"view_id": "html", "html_template": init_html_tmpl},
         {"view_id": "ner_manual", "labels": ["CONCEPT"]},
-        {"view_id": "html", "html": f"<hr><b>{questions[1]}</b>"},
+        {"view_id": "html", "html": f"<hr><b>{questions[1]}</b>\n<p>{q2_key}"},
         {"view_id": "choice", "text": None, "options": q2_labels},
         {"view_id": "html", "html": f"<hr><b>{questions[2]}</b>"},
         {"view_id": "text_input", "field_rows": 3},
@@ -136,7 +206,11 @@ def concept_eval_recipe(
         "ner_manual_highlight_chars": True,
         "custom_theme": {"cardMaxWidth": "70%"},
         "allow_work_stealing": False,
+        "show_stats": False,  # removing since accept is the only option
     }
+    ### Add instructions if provided
+    if instruct and pathlib.Path(instruct).is_file():
+        config |= {"instructions": instruct}
 
     # Define custom recipe methods
     ## Get language example totals
@@ -174,6 +248,8 @@ def concept_eval_recipe(
         """
         q1_spans = eg.get("spans", [])
         q2_selected = eg.get("accept", [])
+        q3_response = eg.get("user_input", "").strip()
+        flagged = eg.get("flagged", False)
 
         # Validate Q1 answer
         if len(q1_spans) == 0 and "missing" not in q2_selected:
@@ -187,9 +263,21 @@ def concept_eval_recipe(
             raise ValueError(
                 "If the concept was omitted in the translation, no selections should be made for Q1"
             )
+        # Validate Q3 answer
+        if "other" in q2_selected and not q3_response:
+            raise ValueError(
+                "If the concept translation error is marked as other,"
+                " then the nature of the error must be documented in Q3"
+            )
+        if flagged and not q3_response:
+            raise ValueError(
+                "If an annotation is flagged, then the issue(s) must be documented in Q3"
+            )
 
     # Setup stream
     stream = get_stream(source)
+    stream.apply(add_lang_name, stream)
+    stream.apply(add_history_text, stream)
     stream.apply(add_tokens, stream)
     stream.apply(set_stream_hashes, stream)
 
