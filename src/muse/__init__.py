47aa7e8252e3c1cca139990f10084de5e440fbce
Laure Thompson <602628+laurejt@users.noreply.github.com>
2026-04-21 15:13:58 -0400

    Increase branch version

--- src/muse/__init__.py
+++ src/muse/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2025, Center for Digital Humanities, Princeton University
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 __all__ = ["__version__", "annotation", "evaluation", "parallel_corpus", "translation"]
